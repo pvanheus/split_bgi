e10b192d67b8f00dfb4780d878a9a8822d0a58f4
Peter van Heusden <pvh@sanbi.ac.za>
2019-08-21 11:15:07 +0200

    * remote seq_io dependency * refactor to be clippy-lint clean

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,10 @@ use flate2::read::GzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::io;
-use std::io::{BufReader, BufWriter};
+use std::io::{BufReader, BufWriter, Error};
 use clap::App;
 
-fn main()  -> io::Result<()> {
-//    let fastq_input_path = "/home/pvh/Data/fastq/SRR8651604.fastq.gz";
-
-    let matches = App::new("split_bgi")
-        .version("0.1")
-        .about("Splits BGISEQ-500 files from Chen et al paper")
-        .author("Peter van Heusden <pvh@sanbi.ac.za>")
-        .args_from_usage(
-            "<INPUT>            '.fastq.gz input file with read 2 followed by read 1"
-        )
-        .get_matches();
-
-    let fastq_input_path = matches.value_of("INPUT").unwrap();
+fn split_bgi(fastq_input_path: &str) -> Result<(), Error> {
     let fastq_file = BufReader::new(File::open(fastq_input_path)?);
     let gzipped_reader = GzDecoder::new(fastq_file)?;
     let reader = Reader::new(gzipped_reader);
@@ -34,15 +22,15 @@ fn main()  -> io::Result<()> {
     let mut fastq_output_1 = Writer::new(GzEncoder::new(BufWriter::new(File::create(read_1_output_path)?), Compression::default()));
     let mut fastq_output_2 = Writer::new(GzEncoder::new(BufWriter::new(File::create(read_2_output_path)?), Compression::default()));
 
-    let mut iter = reader.records();
-    let mut read_2_count = 0;
+    let iter = reader.records();
+//    let mut read_2_count = 0;
     let mut read_1_count = 0;
-    while let Some(result) = iter.next() {
+    for result in iter {
         let record = result.unwrap();
         let description = record.desc().unwrap();
         if description.ends_with('2') {
-            read_2_count += 1;
-            fastq_output_2.write_record(&record);
+//            read_2_count += 1;
+            if let Err(e) = fastq_output_2.write_record(&record) { return Err(e) }
         } else {
             read_1_count += 1;
             let id = record.id();
@@ -55,8 +43,23 @@ fn main()  -> io::Result<()> {
                 record.seq(),
                 record.qual()
             );
-            fastq_output_1.write_record(&new_record);
+            if let Err(e) = fastq_output_1.write_record(&new_record) { return Err(e) }
         }
+
     }
     Ok(())
+}
+
+fn main()  -> io::Result<()> {
+    let matches = App::new("split_bgi")
+        .version("0.1")
+        .about("Splits BGISEQ-500 files from Chen et al paper")
+        .author("Peter van Heusden <pvh@sanbi.ac.za>")
+        .args_from_usage(
+            "<INPUT>            '.fastq.gz input file with read 2 followed by read 1"
+        )
+        .get_matches();
+
+    let fastq_input_path = matches.value_of("INPUT").unwrap();
+    split_bgi(fastq_input_path)
 }
